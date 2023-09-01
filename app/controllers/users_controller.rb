f546c2ec9e19481733ca696e7c2d950ed2dc5371
Alejandro Ramirez <josealejandroramz@gmail.com>
2023-08-31 23:49:14 -0400

    Feat: added action create user with validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,24 @@ class UsersController < ApplicationController
         @users = User.all.order(id: :asc)
     end
 
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+    
+        if @user.save
+          redirect_to users_path, notice: 'Usuario creado con éxito'
+        else
+          render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:type_person, :identification, :issuance_date, :expiration_date, :name, :email, :main_phone, :secondary_phone)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  post '/users', to: 'users#create'
+  get '/users/new', to: 'users#new', as: :new_user
   get '/users', to: 'users#index'
 
 end
