a64b8d333a269b59fc43307f4ca1f4a1e5f50aab
Alejandro Ramirez <josealejandroramz@gmail.com>
2023-09-02 02:27:13 -0400

    Feat: added multi-language configuration

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module SiscoBank
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # idiomas disponibles
+    config.i18n.available_locales = [:en, :es]
+    
+    # idioma por defecto
+    config.i18n.default_locale = :es
   end
 end
