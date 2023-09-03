f84bbd8a91d1db0fc7ffcc0223b3251beeeac31e
Alejandro Ramirez <josealejandroramz@gmail.com>
2023-09-02 23:12:29 -0400

    Feat: added migration type_people

---
+++ app/models/type_person.rb
@@ -0,0 +1,2 @@
+class TypePerson < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_31_170131) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_03_030734) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "type_people", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "type_person", null: false
